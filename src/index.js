3b536687dc636cab9dabb80966b0ad71738446b2
alex-beaverg <a.beaverg@gmail.com>
2020-10-06 21:23:30 +0300

    docs: upload task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = str;
+  let etalon = '';
+  let pare = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+        pare.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  } 
+  while (result !== etalon) {
+        etalon = result;
+    for (let item of pare) {
+                if (result.indexOf(item) !== -1) {
+                        result = result.slice(0, result.indexOf(item)) + result.slice(result.indexOf(item) + 2);       
+                }
+    }
+  } 
+  if (result === '') return true;
+  else return false;
 }
